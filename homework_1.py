d60a91db17c5fbb1ddaa25eecbf87770139910c7
s.berdnikov <s.berdnikov@robokassa.ru>
2024-02-19 01:21:23 +0300

    frst homwrk

---
+++ homework_1.py
@@ -0,0 +1,23 @@
+import pytest
+from selene import browser, be, have
+
+
+@pytest.fixture(scope='session')
+def browser_set():
+    browser.config.browser_name = 'firefox'
+    browser.config.hold_browser_open = True
+    browser.config.window_width = 800
+    browser.config.window_height = 600
+    yield
+
+
+def test_search_positive(browser_set):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_search_negative(browser_set):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('hweugiuwyebgu').press_enter()
+    browser.element('[aria-level="3"]').should(have.text('не найдено'))
