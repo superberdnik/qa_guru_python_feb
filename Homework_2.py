7c5e320cde255541ea3d44139f2b84bd9a428aee
s.berdnikov <s.berdnikov@robokassa.ru>
2024-02-29 00:33:12 +0300

    scnd homwrk

--- Homework_2.py
+++ Homework_2.py
@@ -7,7 +7,7 @@ def test_greeting():
     name = "Анна"
     age = 25
     # TODO Сформируйте нужную строку
-    print (output = "name" + "age")
+    output = "name" + "age"
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
 
